3ad1e7b844afc7a7b127ab91ec16f0f90b8c7bf1
lucasan93 <lucasma3@al.insper.edu.br>
2022-03-27 14:58:07 -0300

    updates defs_fidcs

--- tests/testthat/test-dados_fidc.R
+++ tests/testthat/test-dados_fidc.R
@@ -3,12 +3,12 @@ test_that("dados_fidc", {
   fidc <- dados_fidc(cnpj  = '24.194.675/0001-87',
                       start = as.Date('2018-10-01'),
                       end   = as.Date('2019-03-01'),
-                      table = 'II')
+                      table = 'I')
 
   testthat::expect_s3_class(fidc,
                             'data.frame')
 
   testthat::expect_equal(ncol(fidc),
-                         7)
+                         17)
 
 })
