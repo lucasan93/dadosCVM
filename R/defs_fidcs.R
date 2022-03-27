c1d0a7758176c2fcfa9f05073be548e70f0b7602
lucasan93 <lucasma3@al.insper.edu.br>
2022-03-27 00:48:45 -0300

    added more data to defs_fidcs

--- R/defs_fidcs.R
+++ R/defs_fidcs.R
@@ -1,4 +1,8 @@
-defs_fidcs <- read.csv("C:/Users/lmarin/Desktop/Research/Arquivos R/concorrentes/defs_fidcs.txt")
+
+
+defs_fidcs <- readr::read_delim("C:/Users/lmarin/Desktop/Research/Arquivos R/concorrentes/defs_fidcs.txt",
+                         delim = "\t", escape_double = FALSE,
+                         trim_ws = TRUE)
 
 usethis::use_data(defs_fidcs, overwrite = TRUE)
 
