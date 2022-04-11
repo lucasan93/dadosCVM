3a42b26fd18ae9b5f28fdead90498e8e42f862af
lucasan93 <lucasma3@al.insper.edu.br>
2022-04-11 10:09:36 -0300

    adds the 'PADRONIZADO' terms to rem

--- R/simpl_fundos.R
+++ R/simpl_fundos.R
@@ -14,8 +14,11 @@ simpl_funds <- function(names){
                                  'cotas', 'privado', 'fundos', 'crédito', 'exterior',
                                  'no', 'renda', 'ações', 'fixa', 'fi', 'credito',
                                  'financeiro', 'fic', 'quotas', 'direitos', 'prazo', 'ie', 'participações',
-                                 'creditórios', 'di', 'longo', 'prev', 'fdo', 'priv', 'invest', 'multiestrategia', 'inv', 'referenciado', 'previdenciário', 'long', 'aplicacao', 'cred', 'ficfi', 'não', 'imobiliário', 'padronizados', 'fdos', 'aplic', 'mult', 'previdência', 'acoes', 'fia', 'fidc', 'creditorios', 'curto', 'créd', 'fii', 'investimentos', 'rf', 'fim', 'cp', 'fmia', 'exclusivo', 'lp', 'multiestrategia', 'am', 'total', 'multi', 'qualificado', 'faq', 'fip', 'yield', 'participacoes', 'fife', 'multissetorial', 'np', 'previdenciario', 'imobiliario', 'moderado', 'incentivado', 'alocação', 'aplicação', 'rv', 'fc', 'performance', 'fof', 'soberano', 'dividendos', 'fmp', 'financ', 'fgts', 'conservador', 'fie', 'multigestor', 'fiq', 'nao', 'exclusive', 'estruturado', 'empresas', 'liquidez', 'públicos', 'títulos', 'fin', 'invest', 'financ', 'financ', 'F A Q'))
+                                 'creditórios', 'di', 'longo', 'prev', 'fdo', 'priv', 'invest', 'multiestrategia', 'inv', 'referenciado', 'previdenciário', 'long', 'aplicacao', 'cred', 'ficfi', 'não', 'imobiliário', 'padronizados', 'fdos', 'aplic', 'mult', 'previdência', 'acoes', 'fia', 'fidc', 'creditorios', 'curto', 'créd', 'fii', 'investimentos', 'rf', 'fim', 'cp', 'fmia', 'exclusivo', 'lp', 'multiestrategia', 'am', 'total', 'multi', 'qualificado', 'faq', 'fip', 'yield', 'participacoes', 'fife', 'multissetorial', 'np', 'previdenciario', 'imobiliario', 'moderado', 'incentivado', 'alocação', 'aplicação', 'rv', 'fc', 'performance', 'fof', 'soberano', 'dividendos', 'fmp', 'financ', 'fgts', 'conservador', 'fie', 'multigestor', 'fiq', 'nao', 'exclusive', 'estruturado', 'empresas', 'liquidez', 'públicos', 'títulos', 'fin', 'invest', 'financ', 'financ', 'F A Q', 'padronizado'))
 
   stringr::str_squish(stringr::str_remove_all(gsub('[[:punct:]]',' ', names), paste(paste0('\\b',rem,'\\b'),collapse = '|')))
 
 }
+
+
+simpl_funds('fundo de investimento em multimercado luca')
